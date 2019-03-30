de108a2e150e681f810c8d053e1444fc4434f18e
CLEMENT PIAT <clement.piat2@gmail.com>
2019-03-31 00:06:13 +0100

    j'ai bottom scroll init

--- messagefun/src/components/Chat.js
+++ messagefun/src/components/Chat.js
@@ -5,14 +5,20 @@ import io from 'socket.io-client';
 import axios from "axios";
 
 class Chat extends Component {
+
     constructor(props) {
         super(props);
-        this.socket = io('http://192.168.43.173:5000');
+        this.socket = io('http://192.168.43.88:5000');
         this.state = {
             messages: []
         }
 
-        axios.get('http://192.168.43.173:4000/messages').then(m => this.setState({messages:m.data}))
+        axios.get('http://192.168.43.88:4000/messages').then(m => {
+            this.setState({messages:m.data});
+            let objDiv = document.getElementById("Chat");
+            objDiv.scrollTop = objDiv.scrollHeight;
+            console.log( objDiv.scrollHeight) 
+    })
 
         this.socket.on('msg', msg => {
             var messages = this.state.messages;
@@ -22,7 +28,11 @@ class Chat extends Component {
         this.onMessageSend = this.onMessageSend.bind(this)
 
     }
-
+    componentDidMount(){
+        let objDiv = document.getElementById("Chat");
+        objDiv.scrollTop = objDiv.scrollHeight;
+        console.log( objDiv.scrollHeight)
+    }
     onMessageSend = message => {
         let newMessage = {
             text: message,
@@ -31,10 +41,14 @@ class Chat extends Component {
         }
         this.socket.emit('msg', newMessage)
     }
-
+    scrollToBottom() {
+        var objDiv = document.getElementById("Chat");
+        objDiv.scrollTop = objDiv.scrollHeight;
+    }
+    
     render() {
         return (
-            <div>
+            <div class="Chat" id="Chat">
                 <Messages currentMember={this.props.user} messages={this.state.messages} />
                 <Input onMessageSend={this.onMessageSend} />
             </div>
