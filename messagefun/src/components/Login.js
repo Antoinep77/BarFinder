85218cdda0c7c1bb9a84a366d356eee723561c40
Badr Youbi Idrissi <badr.youbi-idrissi@student.ecp.fr>
2019-03-30 21:03:26 +0100

    Login marche

--- messagefun/src/components/Login.js
+++ messagefun/src/components/Login.js
@@ -4,7 +4,7 @@ class Login extends Component {
     constructor(props){
         super(props);
         this.state = {
-            username: undefined
+            username: ""
         }
     }
 
@@ -12,7 +12,7 @@ class Login extends Component {
     return (<div>
               <div>Welcome to the chat, please enter your username</div>
               <input value={this.state.username} onChange={event => this.setState({username:event.target.value})}/>
-              <button onSubmit = {()=>this.props.onSubmit(this.state.username)}>Validate</button>
+              <button onClick = {()=>this.props.onSubmit(this.state.username)}>Validate</button>
     </div>
 );
   }
