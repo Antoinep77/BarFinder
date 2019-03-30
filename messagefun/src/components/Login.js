46c552d3c82a76abd8c56043b38dd12e41b13242
AntoineParis <antoine.paris@student.ecp.fr>
2019-03-30 20:59:50 +0100

    good login

--- messagefun/src/components/Login.js
+++ messagefun/src/components/Login.js
@@ -1,7 +1,21 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-export default class Login extends Component{
-    render(){
-        return null
+class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: undefined
+        }
     }
+
+  render() {
+    return (<div>
+              <div>Welcome to the chat, please enter your username</div>
+              <input value={this.state.username} onChange={event => this.setState({username:event.target.value})}/>
+              <button onSubmit = {()=>this.props.onSubmit(this.state.username)}>Validate</button>
+    </div>
+);
+  }
 }
+
+export default Login;
