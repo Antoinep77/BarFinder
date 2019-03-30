55c98950e50dd85e58f7b44a9d0cf268b70f67af
Badr Youbi Idrissi <badr.youbi-idrissi@student.ecp.fr>
2019-03-30 20:58:30 +0100

    boiler plate

--- messagefun/src/App.js
+++ messagefun/src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import Chat from './components/Chat';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      user : undefined
+    }
+  }
+  onSubmit(user){
+    this.setState({
+      user
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.state.user ? <Chat user={this.state.user}/> : <Login onSubmit={this.onSubmit}/>}
         </header>
       </div>
     );
