e2c16cf2a92205c80bcb87908ba2a798788e7f88
AntoineParis <antoine.paris@student.ecp.fr>
2019-03-30 23:43:51 +0100

    add checkwords function

--- Server/checkMissions/checkWord.js
+++ Server/checkMissions/checkWord.js
@@ -3,7 +3,8 @@ var missions = require('../models/mission');
 module.exports = (msgText,username) => {
     missions.findOne({current:true, username}).then( m=>{
         if(m ){
-            var regexpPattern = new RegExp("[\(\)\[\]\"'\-,;:\/!\?]?"+m.word +"[\(\)\[\]\"'\-,;:\/!\?]?")
+            var regexpPattern = new RegExp("[\(\)\[\]\"'\-,;:\/!\?]?"+m.word +"[\(\)\[\]\"'\-,;:\/!\?]?");
+            
         }
     })
 }
