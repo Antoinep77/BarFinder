f75058fe3d99f6ca147c007a445d0b754ed1c747
Badr Youbi Idrissi <badr.youbi-idrissi@student.ecp.fr>
2019-03-30 21:45:52 +0100

    Chat works

--- messagefun/src/components/Chat.js
+++ messagefun/src/components/Chat.js
@@ -1,7 +1,52 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import Messages from "./Messages";
+import Input from "./Input";
 
-export default class Login extends Component{
-    render(){
-        return null
+class Chat extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            messages: [
+                {
+                    id:1,
+                    text: "Hi clément",
+                    member: {
+                        username: "badrdr"
+                    }
+                },
+                {
+                    id:2,
+                    text: "Hi Badr !",
+                    member: {
+                        username: "clemclem"
+                    }
+                }
+            ],
+            member: {
+                username: "badrdr"
+            }
+        }
+    }
+
+    onMessageSend = message =>{
+        let newMessage = {
+            id:this.state.messages.length+1,
+            text:message,
+            member:this.state.member
+        }
+        this.setState(prevState => ({
+            messages : [...prevState.messages, newMessage]
+        }))
+    }
+
+    render() {
+        return (
+        <div id="chat">
+            <Messages currentMember={this.state.member} messages={this.state.messages}/>
+            <Input onMessageSend={this.onMessageSend}/>
+        </div>
+        )
     }
 }
+
+export default Chat;
