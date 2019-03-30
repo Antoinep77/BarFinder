e48423d81ca26077db3b1ff89950df0cd105300c
CLEMENT PIAT <clement.piat2@gmail.com>
2019-03-30 18:38:48 +0100

    add on_join handler

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask_socketio import join_room, leave_room
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -17,6 +19,20 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
 
+@socketio.on('join')
+def on_join(data):
+    print(str(data))
+    #username = data['username']
+    #room = data['room']
+    #join_room(room)
+    #send(username + ' has entered the room.', room=room)
+
+# @socketio.on('leave')
+# def on_leave(data):
+#     username = data['username']
+#     room = data['room']
+#     leave_room(room)
+#     send(username + ' has left the room.', room=room)
 
 
 
