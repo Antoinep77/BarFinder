db9b740d2f872efed4cbead8a8543839635b4479
AntoineParis <antoine.paris@student.ecp.fr>
2019-03-31 06:26:41 +0200

    block work

--- Server/missions/checkWord.js
+++ Server/missions/checkWord.js
@@ -10,8 +10,8 @@ module.exports = (msg) => {
             var ponctPattern = "[\\(\\)\\[\\]\"'\\-,;:\\/!\\?\\. ]"
             var regexpPattern = new RegExp("(^|"+ponctPattern+")"+m.word.toLowerCase()+"("+ponctPattern+"|$)");
             if(regexpPattern.test(msgText.toLowerCase())){
-                return missions.findByIdAndUpdate(m._id,{trigger_msg_id:msg._id,current:false,point, date: new Date()})
-
+                return missions.findByIdAndUpdate(m._id,{trigger_msg_id:msg._id,current:false,point, date: new Date()},{  returnNewDocument:true})
+                        .then(m=>m)
             }
         }
         return null
