0dceacdb217a59e73097de82d79a9daf30042441
Badr Youbi Idrissi <badr.youbi-idrissi@student.ecp.fr>
2019-03-31 05:05:19 +0200

    mission

--- messagefun/src/App.js
+++ messagefun/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Login from './components/Login';
 import Chat from './components/Chat';
 import axios from 'axios';
+import io from 'socket.io-client';
 
 class App extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.socket = io('http://192.168.43.173:5000');
     this.state = {
       user: undefined,
       animal : undefined,
       mission : undefined
     }
+    this.socket.on('mission', mission=>{console.log(mission);this.setState({mission})})
+    this.socket.on('mission_complete', mission=>{window.alert("Mission complete !")})
   }
   onSubmit = (user,animal) => {
     this.setState({
@@ -20,8 +24,7 @@ class App extends Component {
         animal: animal
       }
     });
-    axios.get('http://192.168.43.173:4000/missions/'+user).then(mission=>{console.log(mission);this.setState({mission})})
-    .catch(console.log)
+    this.socket.emit('join', user);
   }
 
   render() {
@@ -32,7 +35,7 @@ class App extends Component {
             <h1>My Chat App</h1>
           </div>
         </header>
-        {this.state.user ? <Chat user={this.state.user} /> : <Login onSubmit={this.onSubmit} />}
+        {this.state.user ? <Chat user={this.state.user} mission={this.state.mission} socket={this.socket}/> : <Login onSubmit={this.onSubmit} />}
       </div>
     );
   }
